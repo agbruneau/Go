bd5c4da3ec7b3952c8949d56c5fa6984eaedb3b1
André-Guy Bruneau <45982482+agbruneau@users.noreply.github.com>
2020-07-05 19:08:50 -0400

    update main.go

--- src/AGB/main.go
+++ src/AGB/main.go
@@ -16,11 +16,7 @@ func main() {
 	go worker(jobs, results)
 	go worker(jobs, results)
 	go worker(jobs, results)
-	go worker(jobs, results)
-	go worker(jobs, results)
-	go worker(jobs, results)
-	go worker(jobs, results)
-
+	
 	for i := 0; i < 50; i++ {
 		jobs <- i
 	}
