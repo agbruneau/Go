7e72dcd1865d88eee54b5ba722846ba0c8f05d1e
André-Guy Bruneau <45982482+agbruneau@users.noreply.github.com>
2021-02-21 08:52:37 -0500

    Update FibBig.go

--- src/AGB/FibBig/fib.go
+++ src/AGB/FibBig/fib.go
@@ -52,7 +52,7 @@ func main() {
 	fmt.Printf("time eclibaced is %s \n", elapsed)
 
 	start = time.Now()
-	fmt.Println("1000000: ", FibonacciBig(1000000))
+	fmt.Println("100000000: ", FibonacciBig(100000000))
 	elapsed = time.Since(start)
 	fmt.Printf("time eclibaced is %s \n", elapsed)
 
